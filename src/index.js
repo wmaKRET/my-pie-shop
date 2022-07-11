200cc944d13b22d8a048ff56cb7d0529f79d198a
wmaKRET <wkret91.2@gmail.com>
2022-07-11 14:38:31 +0200

    create data.JSON file (as database), create Context.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
+import { ContextProvider } from './Context'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>
 )
