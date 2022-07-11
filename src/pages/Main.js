7e12628fed9aaa83b1d0f10b0a27fc7489607f7b
wmaKRET <wkret91.2@gmail.com>
2022-07-11 14:38:10 +0200

    create Main page structure + styling

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,41 @@ import React from "react"
 
 function Main(){
     return (
-        <main>
-            <h1>main page</h1>
+        <main className="main">
+            <h2 className="main__title">Try something new!</h2>
+            <div className="main__random">
+                <img 
+                    src="/img/pie_test.jpg" 
+                    alt="pie"
+                    className="main__random-img"
+                />
+                <img 
+                    src="/img/pie_test.jpg" 
+                    alt="pie"
+                    className="main__random-img"
+                />
+                <img 
+                    src="/img/pie_test.jpg" 
+                    alt="pie"
+                    className="main__random-img"
+                />
+            </div>
+            <h2 className="main__title">Today's special!</h2>
+            <div className="main__special-deal">
+                <img 
+                    src="/img/pie_test.jpg" 
+                    alt="pie"
+                    className="main__special-deal-img"
+                />
+            </div>
+            <h2 className="main__title">Order again!</h2>
+            <div className="main__last-order">
+            <img 
+                    src="/img/pie_test.jpg" 
+                    alt="pie"
+                    className="main__last-order-img"
+                />
+            </div>
         </main>
     )
 }
