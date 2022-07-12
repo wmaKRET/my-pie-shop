677cff72503a3e84f69a755694edb6e0a03d0cb0
wmaKRET <wkret91.2@gmail.com>
2022-07-12 14:48:53 +0200

    add useHover effect for ProductsImage on hover

--- src/components/images/ProductsImage.js
+++ src/components/images/ProductsImage.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+import useHover from "../../hooks/useHover"
 
 function ProductsImage({ item }){
+    const [hovered, ref] = useHover()
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        setVisible(true)
+    }, [])
+
+    const isHovered = hovered ? ' hovered' : ''
+
     return (
-        <div className="products__card">
-            <h3 className="products__card-title">{item.name}</h3>
-            <h2 className="products__card-price">$ {item.price}</h2>
-            <p className="products__card-ingredients">{item.ingredients}</p>
+        <div 
+            className={visible ? "products__card visible" : "products__card"}
+            ref={ref}
+        >   
+            <h3 className={`products__card-title${isHovered}`}>{item.name}</h3>
+            <h2 className={`products__card-price${isHovered}`}>$ {item.price}</h2>
+            <p className={`products__card-ingredients${isHovered}`}>{item.ingredients}</p>
             <img 
                 src={item.img_url} 
                 alt={item.name}
